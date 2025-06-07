ca262430c81fbe8a25cc2613a66a3c6d4a7eb783
Your Name <you@example.com>
2025-06-07 18:01:23 +0300

    Fix: Configure Metro for correct file watching

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,16 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    nodeModulesPaths: [path.resolve(__dirname, './node_modules')],
+  },
+  projectRoot: __dirname,
+  watchFolders: [
+    __dirname,
+    path.resolve(__dirname, 'app'), // Если у вас есть исходный код в папке `app`
+    path.resolve(__dirname, 'src'), // Если у вас есть исходный код в папке `src`
+  ],
+};
 
 module.exports = mergeConfig(getDefaultConfig(), config); 
